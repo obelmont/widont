8436bbf2275ceab95cfba7d901e3b0f096c4b004
Oliver Belmont <obelmont@oliverbelmont.com>
2018-01-25 15:34:48 +1100

    include wysiwyg
    Signed-off-by: Oliver Belmont <obelmont@oliverbelmont.com>

--- index.php
+++ index.php
@@ -14,12 +14,23 @@ function widont($str = '')
 
 function my_acf_load_value( $value, $post_id, $field )
 {
-    // run widont on value.
+    // run widont on value
     $value = widont($value);
     return $value;
+}
+
+
+function my_acf_load_value_two( $value, $post_id, $field )
+{
+    // run widont on value
+    var_dump($value);
+    $value = widont($value);
+    return $value;
+
 }
 
 
 // acf/load_value - filter for every value load
 add_filter('acf/load_value/type=text', 'App\my_acf_load_value', 10, 3);
 add_filter('acf/load_value/type=textarea', 'App\my_acf_load_value', 10, 3);
+add_filter('acf/load_value/type=wysiwyg', 'App\my_acf_load_value_two', 10, 3);
