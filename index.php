0ca7d20a532bd282e7fe604a98c38d8598480cd5
Oliver Belmont <obelmont@oliverbelmont.com>
2018-01-25 15:05:24 +1100

    Init
    Signed-off-by: Oliver Belmont <obelmont@oliverbelmont.com>

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+function widont($str = '')
+{
+  $str = rtrim($str);
+  $space = strrpos($str, ' ');
+  if ($space !== false)
+  {
+    $str = substr($str, 0, $space).'&nbsp'.substr($str, $space + 1); //;
+  }
+  return $str;
+}
+
+
+function my_acf_load_value( $value, $post_id, $field )
+{
+    // run widont on value.
+    $value = widont($value);
+    return $value;
+}
+
+
+// acf/load_value - filter for every value load
+add_filter('acf/load_value/type=text', 'App\my_acf_load_value', 10, 3);
+add_filter('acf/load_value/type=textarea', 'App\my_acf_load_value', 10, 3);
